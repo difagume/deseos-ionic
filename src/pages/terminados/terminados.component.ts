43394931ddb735960e7eab162abf2f0357696cef
Diego Fabricio <difagume@gmail.com>
2018-02-25 11:29:24 -0500

    Presentación de info cuando no hay ninguna lista

--- src/pages/terminados/terminados.component.ts
+++ src/pages/terminados/terminados.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ListaDeseosService } from '../../app/services/lista-deseos.service';
+import { NavController } from 'ionic-angular';
+import { DetalleComponent } from '../detalle/detalle.component';
 
 @Component({
     selector: 'app-terminados',
     templateUrl: './terminados.component.html'
 })
 export class TerminadosComponent implements OnInit {
-    constructor() { }
+    constructor(
+        public _listaDeseos: ListaDeseosService,
+        public navCtrl: NavController
+    ) { }
 
     ngOnInit() { }
+
+    verDetalle(lista, index) {
+        this.navCtrl.push(DetalleComponent, { lista, index });
+    }
 }
