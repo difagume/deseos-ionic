a875fdec5cb63c8e773397c3d9c821833ef4dba6
Diego Fabricio <difagume@gmail.com>
2018-02-25 00:48:31 -0500

    Pantalla de detalle y sus funciones

--- src/pages/detalle/detalle.component.ts
+++ src/pages/detalle/detalle.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Lista, ListaItem } from '../../app/clases/index';
+import { ListaDeseosService } from '../../app/services/lista-deseos.service';
+
 
 @Component({
     selector: 'app-detalle',
@@ -8,15 +11,21 @@ import { NavController, NavParams } from 'ionic-angular';
 export class DetalleComponent implements OnInit {
 
     index: number;
-    lista: any;
+    lista: Lista;
 
     constructor(
         public navCtrl: NavController,
-        public navParams: NavParams
+        public navParams: NavParams,
+        public _listaDeseos: ListaDeseosService
     ) {
         this.index = this.navParams.get("index");
         this.lista = this.navParams.get("lista");
     }
 
     ngOnInit() { }
+
+    actualizar(item: ListaItem) {
+        item.completado = !item.completado;
+        this._listaDeseos.actualizarData();
+    }
 }
